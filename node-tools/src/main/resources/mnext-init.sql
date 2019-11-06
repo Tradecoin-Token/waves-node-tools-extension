05c0191c9cd6657886444bcb997ddf965c24feff
Valeriy Gavrilin <yoba123@yandex.ru>
2019-11-06 13:17:21 +0300

    Migration util

--- node-tools/src/main/resources/mnext-init.sql
+++ node-tools/src/main/resources/mnext-init.sql
@@ -9,18 +9,9 @@ create table if not exists payouts
     id                 int4 primary key auto_increment not null,
     from_height        int4                            not null,
     to_height          int4                            not null,
-    reward             int8                            not null,
-    generating_balance int8                            not null,
-    active_leases      longvarbinary                   not null,
-    tx_id              char(44),
-    tx_height          int4,
-    confirmed          bool                            not null default (false)
+    amount             int8                            not null,
+    generating_balance int8                            not null
 );
-
-alter table if exists payouts
-    alter column reward rename to amount;
-alter table if exists payouts
-    drop column active_leases, tx_id, tx_height, confirmed;
 
 create table if not exists payout_transactions
 (
